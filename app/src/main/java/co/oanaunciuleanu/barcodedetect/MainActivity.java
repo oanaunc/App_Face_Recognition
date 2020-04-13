557883c4cd28439962715299e08852ded3abd4be
Unciuleanu Oana <oanaunciuleanu@gmail.com>
2020-04-13 15:37:21 +0300

    fix

--- app/src/main/java/co/oanaunciuleanu/barcodedetect/MainActivity.java
+++ app/src/main/java/co/oanaunciuleanu/barcodedetect/MainActivity.java
@@ -2,7 +2,6 @@ package co.oanaunciuleanu.barcodedetect;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -23,7 +22,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
